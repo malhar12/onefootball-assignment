4f66e87edae8864f14e1063ec552ef11e39756dd
Malhar Deshpande <malhar.deshpande@thoughtworks.com>
2021-07-22 12:44:12 +0530

    removed unnecessary cypress tests, added e2e cypress test for user journey

---
+++ cypress/integration/examples/player-search-journey.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+context('Player Search Journey', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('should show the details of the Player, if the player is active', () => {
+        cy.get('.player-input-box')
+            .type('fabio').should('have.value', 'fabio');
+        cy.get('.submit-button')
+            .click();
+        cy.get('.search-form').should('have.descendants', '.player-details');
+    });
+
+    it('should show that the player is not active, if the player is not active', () => {
+        cy.get('.player-input-box')
+            .type('giorgio').should('have.value', 'giorgio');
+        cy.get('.submit-button')
+            .click();
+        cy.get('.search-form').should('have.descendants', '.error-message');
+    });
+
+    it('should invalid player id message, if the player id is invalid', () => {
+        cy.get('.player-input-box')
+            .type('abcd#').should('have.value', 'abcd#');
+        cy.get('.submit-button')
+            .click();
+        cy.get('.search-form').should('have.descendants', '.error-message');
+    });
+})

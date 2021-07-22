2c1581f9cb943847322f59578abe6f6ff86bd15c
Malhar Deshpande <malhar.deshpande@thoughtworks.com>
2021-07-22 18:26:07 +0530

    UX Design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'player-archive';
   activePlayerDetails: FormEventDetails;
+  activePlayerStatKeys: Array<string>;
   constructor() {
     this.activePlayerDetails = {
       status: false,
       details: null,
       message: ''
     };
+    this.activePlayerStatKeys = [];
   }
 
   displayPlayerDetails(event: FormEventDetails): void {
     this.activePlayerDetails = event;
+
+    if(this.activePlayerDetails.details && this.activePlayerDetails.details.stats)
+      this.activePlayerStatKeys = Object.keys(this.activePlayerDetails.details?.stats);
   }
 }
